5dbfe448563f83da41502e82ccedadb37371e1bc
Lukas Pistrol <lukas@pistrol.com>
2022-10-04 17:56:47 +0200

    skip package updates

--- .github/scripts/tests.sh
+++ .github/scripts/tests.sh
@@ -15,5 +15,6 @@ export LC_CTYPE=en_US.UTF-8
 
 set -o pipefail && arch -"${ARCH}" xcodebuild  \
            -scheme CodeEditTextView \
+           -skipPackageUpdates \
            -destination "platform=macos,arch=${ARCH}" \
            test
