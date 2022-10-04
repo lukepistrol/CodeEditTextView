2d05af69c27ca4aaa112c1438a2ace504c005082
Lukas Pistrol <lukas@pistrol.com>
2022-10-04 19:19:13 +0200

    update

--- .github/scripts/tests.sh
+++ .github/scripts/tests.sh
@@ -15,7 +15,6 @@ export LC_CTYPE=en_US.UTF-8
 
 set -o pipefail && arch -"${ARCH}" xcodebuild  \
            -scheme CodeEditTextView \
-           -disableAutomaticPackageResolution \
            -derivedDataPath ".build" \
            -destination "platform=macos,arch=${ARCH}" \
            test
